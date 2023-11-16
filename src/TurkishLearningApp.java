b46eeef109a167fb7b61ad74277fe2eb436dde7e
jeddiprog <jeddijeddi10@gmail.com>
2023-11-16 15:26:14 +0100

    version 1

--- src/TurkishLearningApp.java
+++ src/TurkishLearningApp.java
@@ -1,78 +1,98 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TurkishLearningApp {
+    private final static Map<String, String> translations = new HashMap<>();
+    private static JLabel turkishLabel;
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame("learn turkish");
+        JFrame frame = new JFrame("Learn Turkish");
         JTabbedPane tabbedPane = new JTabbedPane();
 
         JPanel listeningPanel = new JPanel();
-        listeningPanel.add(new JLabel("Listening exercices"));
+        listeningPanel.add(new JLabel("Listening exercises"));
         tabbedPane.addTab("Écoute", listeningPanel);
 
         JPanel writingPanel = new JPanel();
-        writingPanel.add(new JLabel("writing exercices"));
+        writingPanel.add(new JLabel("Writing exercises"));
         tabbedPane.addTab("Écriture", writingPanel);
 
         JPanel speakingPanel = new JPanel();
-        speakingPanel.add(new JLabel("Speaking exercices"));
+        speakingPanel.add(new JLabel("Speaking exercises"));
         tabbedPane.addTab("Expression Orale", speakingPanel);
 
         JPanel readingPanel = new JPanel();
-        readingPanel.add(new JLabel("Lecture exercices"));
+        readingPanel.add(new JLabel("Lecture exercises"));
         tabbedPane.addTab("Lecture", readingPanel);
 
-        JTabbedPane learningTabbedPane = new JTabbedPane();
+        JTabbedPane learningConceptsTabbedPane = new JTabbedPane();
         JPanel alphabetPanel = new JPanel();
-        alphabetPanel.add(new JLabel("Learn turkish alphabet"));
-        learningTabbedPane.addTab("Alphabet", alphabetPanel);
+        alphabetPanel.add(new JLabel("Learn Turkish alphabet"));
+        learningConceptsTabbedPane.addTab("Alphabet", alphabetPanel);
 
         JPanel colorsPanel = new JPanel();
-        colorsPanel.add(new JLabel("learn turkish colors"));
-        learningTabbedPane.addTab("Colors", colorsPanel);
+        colorsPanel.add(new JLabel("Learn Turkish colors"));
+        learningConceptsTabbedPane.addTab("Colors", colorsPanel);
 
         JPanel numbersPanel = new JPanel();
-        numbersPanel.add(new JLabel("learn turkish numbers"));
-        learningTabbedPane.addTab("Numbers", numbersPanel);
+        numbersPanel.add(new JLabel("Learn Turkish numbers"));
+        learningConceptsTabbedPane.addTab("Numbers", numbersPanel);
 
-        JPanel timePanel = new JPanel();
-        timePanel.add(new JLabel("learn the hour in turkish"));
-        learningTabbedPane.addTab("Hour", timePanel);
+        JPanel timePanel = new JPanel(new GridLayout(0, 2));
+        learningConceptsTabbedPane.addTab("Hour", timePanel);
+
+        turkishLabel = new JLabel("", SwingConstants.CENTER);
+
+        addTranslationToMap("Mañana", "sabah");
+        addTranslationToMap("tarde", "öğleden sonra");
+        addTranslationToMap("noche", "gece");
+        addTranslationToMap("medianoche", "gece yarısı");
+        addTranslationToMap("amanecer", "şafak");
+        addTranslationToMap("oscuridad", "alacakaranlık");
+        addTranslationToMap("noche, tarde", "akşam");
+        addTranslationToMap("hora del día", "gündüz");
+
+        for (String spanishWord : translations.keySet()) {
+            JButton spanishButton = new JButton(spanishWord);
+            spanishButton.addActionListener(new TranslationActionListener(spanishWord));
+            timePanel.add(spanishButton);
+        }
+
+        timePanel.add(turkishLabel);
+
+        learningConceptsTabbedPane.addTab("Time", timePanel);
 
         JPanel weatherPanel = new JPanel();
-        weatherPanel.add(new JLabel("learn the weather vocabulary in turkish"));
-        learningTabbedPane.addTab("Weather", weatherPanel);
+        weatherPanel.add(new JLabel("Learn the weather vocabulary in Turkish"));
+        learningConceptsTabbedPane.addTab("Weather", weatherPanel);
 
-        tabbedPane.addTab("learning", learningTabbedPane);
+        tabbedPane.addTab("Learning", learningConceptsTabbedPane);
 
         frame.add(tabbedPane, BorderLayout.CENTER);
         frame.setSize(600, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        colorsPanel.setLayout(new GridLayout(12, 1));
-        colorsPanel.add(new JLabel("learn colors in turkish and spanish"));
-
-        addColorToPanel(colorsPanel, "red", "kırmızı", "rojo");
-        addColorToPanel(colorsPanel, "orange", "turuncu", "naranjo");
-        addColorToPanel(colorsPanel, "yellow", "sarı", "amarillo");
-        addColorToPanel(colorsPanel, "green", "yeşil", "verde");
-        addColorToPanel(colorsPanel, "blue", "mavi", "azul");
-        addColorToPanel(colorsPanel, "purple", "mor", "morado");
-        addColorToPanel(colorsPanel, "pink", "pembe", "rosa");
-        addColorToPanel(colorsPanel, "white", "beyaz", "blanco");
-        addColorToPanel(colorsPanel, "black", "siyah", "negro");
-        addColorToPanel(colorsPanel, "grey", "gri", "gris");
-        addColorToPanel(colorsPanel, "brown", "kahverengi", "castaño");
-
-        learningTabbedPane.addTab("Colors", colorsPanel);
-
-    }
-    private static void addColorToPanel(JPanel panel, String english, String turkish, String spanish) {
-        JPanel colorPanel = new JPanel(new GridLayout(1, 3));
-        colorPanel.add(new JLabel(english));
-        colorPanel.add(new JLabel(turkish));
-        colorPanel.add(new JLabel(spanish));
-        panel.add(colorPanel);
     }
 
+    private static void addTranslationToMap(String spanish, String turkish) {
+        translations.put(spanish, turkish);
+    }
+
+    private static class TranslationActionListener implements ActionListener {
+        private final String spanishWord;
+
+        public TranslationActionListener(String spanishWord) {
+            this.spanishWord = spanishWord;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String turkishTranslation = translations.get(spanishWord);
+            turkishLabel.setText(turkishTranslation);
+        }
+    }
 }
